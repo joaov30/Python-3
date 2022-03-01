5f061de3b148d984e0b9f42a25acac4dc0b8ad7c
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2022-03-01 16:32:30 -0300

    ex 3, 4

--- PythonExercicios/ex003.py
+++ PythonExercicios/ex003.py
@@ -2,4 +2,4 @@ numero1 = int(input('Digte o Primeiro numero: '))
 numero2 = int(input('Digite o Segundo numero'))
 resultado = numero1 + numero2
 
-print('O Resultado da soma é: {}'.format(resultado))
+print(f'A Soma entre {numero1} e {numero2} é: {resultado}')
---
+++ PythonExercicios/ex004.py
@@ -0,0 +1,3 @@
+info = input('Digite alguma coisa: ')
+print('Você digitou uma lerta = ', info.isalpha())
+print('Você digitou um numero = ', info.isalnum())

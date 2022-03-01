9128f535ef9eb3112e8c6dde10e885c861ee9263
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2022-03-01 15:47:54 -0300

    Create ex003.py

---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digte o Primeiro numero: '))
+numero2 = int(input('Digite o Segundo numero'))
+resultado = numero1 + numero2
+
+print('O Resultado da soma é: {}'.format(resultado))

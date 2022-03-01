094ca71e29da3208c515f2105ee970ca3588ac4f
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2022-03-01 18:04:17 -0300

    Update ex004.py

--- PythonExercicios/ex004.py
+++ PythonExercicios/ex004.py
@@ -1,3 +1,6 @@
 info = input('Digite alguma coisa: ')
-print('Você digitou uma lerta = ', info.isalpha())
-print('Você digitou um numero = ', info.isalnum())
+print('O tipo primitivo desse valor é ', type(info))
+print(f'Você digitou {info} possui apenas letras ? = ', info.isalpha())
+print('So tem espacos', info.isspace())
+print(f'Você digitou {info} possui apenas numeros ? = ', info.isnumeric())
+print(f'Você digitou {info}, Possui letras e numeros? ', info.isalnum())

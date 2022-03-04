45862567ccc6298bd136e0eb2904263512e022f3
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2022-03-04 00:39:32 -0300

    Create ex009.py

---
+++ PythonExercicios/ex009.py
@@ -0,0 +1,11 @@
+nome = str(input('qual o seu nome '))
+print('Prazer em te conhecer {:-^20}! \n'.format(nome))
+
+n = int(input(' Bem vindo a Tabuada, digite um numero para ver o resultado: '))
+multiplicando = 0
+cont = 0
+
+while cont <= 10:
+    print(n * multiplicando)
+    multiplicando = multiplicando + 1
+    cont = cont + 1

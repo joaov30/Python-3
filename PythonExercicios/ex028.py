c8da2c7fbb519e284207b2754ca0a70e4a00c574
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2022-05-03 17:13:15 -0300

    Update ex028.py

--- PythonExercicios/ex028.py
+++ PythonExercicios/ex028.py
@@ -1,12 +1,19 @@
 import random
-print('Bem vindo ao ogo de advinhação!!')
+from time import sleep
+
+print('-----' * 20)
+print('Bem vindo ao jogo de advinhação!!')
+print('-----' * 20)
+print('Estou aguardando sua resposta...\n')
 chute = int(input('Digite um numero entre 0 e 5: '))
+print('PROCESSANDO....')
 numeros = random.randrange(6)
 
+sleep(3)
 
 if chute > 5 or chute < 0:
     print('Digite um valor entre 0 e 5!')
-    
+
 if numeros == chute:
     print('VOCE ACERTOU PARABENS!!')
     print(numeros)
